b9c00322b8322f9f74be616ca4ed809b145bed6b
Vu Nguyen <135572888+VuNguyen123456@users.noreply.github.com>
2025-07-20 04:46:20 +0000

    s

---
+++ Leet code/236. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+
+        if root == p or root == q:
+            return root  # ✔️ Found p or q
+
+        # Recursively search in left and right subtrees
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root  # ✔️ p and q found in different subtrees so current root is the LCA
+
+        return left if left else right  # Return the non-null result from the left or right subtree
+        # So that it saves the result of the subtree that has p or q and return it to the parent node
