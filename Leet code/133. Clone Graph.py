9c66961db309430e313239e52a87cfb81bef29b2
Vu Nguyen <135572888+VuNguyen123456@users.noreply.github.com>
2025-08-19 21:26:16 +0000

    s

--- Leet code/133. Clone Graph.py
+++ Leet code/133. Clone Graph.py
@@ -5,7 +5,7 @@ class Node(object):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+#################################################DFS
 class Solution(object):
     def cloneGraph(self, node):
         """
@@ -24,4 +24,34 @@ class Solution(object):
             return copy
 
         return dfs(node) if node else None
+
+
+####################################################BFS
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        oldToNew = {}
+        #1st node: create deep copy and add it into hashmap
+        oldToNew[node] = Node(node.val)
+
+        # BFS go though every node level by level: append into queue if they are current node neighbor to check further
+        q = deque([node])
+        while q:
+            cur = q.popleft()
+            for nei in cur.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                # Append nei into new Node neighbors:
+                oldToNew[cur].neighbors.append(oldToNew[nei])
+        return oldToNew[node]
 
