99cb3dbbeaf3ff8a893d66fc1d3edc7b22009147
Vu Nguyen <135572888+VuNguyen123456@users.noreply.github.com>
2025-12-21 21:48:22 +0000

    s

--- Leet code/Subarray Sum Equals K.py
+++ Leet code/Subarray Sum Equals K.py
@@ -25,3 +25,21 @@ class Solution(object):
           prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
 
       return result              # Return total count of valid subarrays
+
+
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         dic = {0 : 1}
+#         res = 0
+#         prefix = 0
+#         for i in range(len(nums)):
+#             prefix += nums[i]
+#             target = prefix - k
+#             if target in dic:
+#                 res += dic[target]
+#                 print(i)
+#             if prefix in dic:
+#                 dic[prefix] += 1
+#             else:
+#                 dic[prefix] = 1
+#         return res
