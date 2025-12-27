f3e3d2191bb8f7e2c3f3bcfcdc46fd7006561d16
Vu Nguyen <135572888+VuNguyen123456@users.noreply.github.com>
2025-12-27 04:35:20 +0000

    s

--- Leet code/46. Permutations.py
+++ Leet code/46. Permutations.py
@@ -1,38 +1,20 @@
 class Solution:
-  def permute(self, nums: List[int]) -> List[List[int]]:
-      ## Iterations:
-      # Basically start with empty list, insert number in according to list
-      # For every new number insert at a new position according to the current list
-      # Ex: [] -> [1] -> [2,1], [1,2] -> [3,2,1], [2,3,1], [2,1,3], [3,1,2], [1,3,2], [1,2,3]
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        def backtrack(index):
+            if n == index: # Compare index because if index = len(nums) => at the last possible element and ret
+                res.append(nums[:])
+                return
 
-      # perms = [[]]
+            for i in range(index , n):
+                nums[i], nums[index] = nums[index], nums[i]
+                backtrack(index + 1)
+                nums[index], nums[i] = nums[i], nums[index]
 
-      # for n in nums:
-      #     new_perms = []
-      #     for p in perms:
-      #         for i in range(len(p) + 1):
-      #             p_copy = p.copy()
-      #             p_copy.insert(i, n)
-      #             new_perms.append(p_copy)
-      #     perms = new_perms
-      # return perms
 
-      ## Backtrack
-      if len(nums) == 1:
-          return [nums[:]]
-      result = []
-
-      for _ in range(len(nums)):
-          n = nums.pop(0)
-          perms = self.permute(nums)
-          for p in perms:
-              p.append(n)
-
-          result.extend(perms)
-          nums.append(n)
-
-      return result
-
+        backtrack(0)
+        return res
 
 # permute([1,2,3])
 # ├── pick 1 → permute([2,3])
